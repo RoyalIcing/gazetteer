9f7a1f607a7fc1b0539ac1e1e1420b97d9a6c9d3
Patrick Smith <patrick@burntcaramel.com>
2019-04-17 22:14:05 +1000

    Remove unused files

--- packages/hybrid-example/src/browser.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { App } from "./root/App";
-
-const shouldHydrate = false;
-
-const el = document.querySelector("#root");
-ReactDOM.render(<App />, el);
